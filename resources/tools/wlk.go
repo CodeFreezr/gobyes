fde3d4d5b5a454e039146850fd31fbd4b0dc80be
CodeFreezr <burkhardt@web.de>
2018-02-04 23:50:38 +0100

    Refactor Folder-Structure

--- resources/tools/wlk.go
+++ resources/tools/wlk.go
@@ -13,6 +13,8 @@ type gofi struct {
 	Name    string
 	Path    string
 	User    string
+	Repo    string
+	Bare    string
 	Size    int64
 	Mode    os.FileMode
 	ModTime time.Time
@@ -21,17 +23,23 @@ type gofi struct {
 
 func main() {
 	searchDir := os.Args[1]
-	ext := ".go"
-	//fileList := make([]string, 0)
+	ext := ".go$"
 
 	fileList := []gofi{}
 	err := filepath.Walk(searchDir, func(path string, f os.FileInfo, err error) error {
 		if !f.IsDir() {
 			reg, err := regexp.MatchString(ext, f.Name())
 			if err == nil && reg {
-				r := strings.Split(path, ".")[0]
+				user := strings.Split(path, ".")[0]
+				bare := strings.Replace(path, user, "", 1)
+				bare = strings.Replace(bare, f.Name(), "", -1)
+				bare = strings.TrimRight(bare, "\\")
+				bare = strings.TrimLeft(bare, ".")
+				repo := strings.Split(bare, "\\")[0]
+				bare = strings.Replace(bare, repo, "", 1)
+				bare = strings.TrimLeft(bare, "\\")
 				//fileList = append(fileList, path)
-				fileList = append(fileList, gofi{f.Name(), path, r, f.Size(), f.Mode(), f.ModTime(), f.IsDir()})
+				fileList = append(fileList, gofi{f.Name(), path, user, repo, bare, f.Size(), f.Mode(), f.ModTime(), f.IsDir()})
 			}
 		} else {
 			//fmt.Println(f.Name(), path)
@@ -45,9 +53,12 @@ func main() {
 	}
 
 	for _, file := range fileList {
-		fmt.Println("File: ", file.Name)
-		fmt.Println("Path: ", file.Path)
+		fmt.Println("----- ")
 		fmt.Println("User: ", file.User)
+		fmt.Println("Repo: ", file.Repo)
+		fmt.Println("File: ", file.Name)
+		fmt.Println("Path: ", file.Bare)
+		fmt.Println("Full: ", file.Path)
 
 	}
 }
