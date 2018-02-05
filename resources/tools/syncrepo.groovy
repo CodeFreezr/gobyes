22ca643f91f93178fa5f190766b8932482f7fdb5
CodeFreezr <burkhardt@web.de>
2018-02-06 00:13:30 +0100

    resync Stuff

--- resources/tools/syncrepo.groovy
+++ resources/tools/syncrepo.groovy
@@ -2,10 +2,7 @@
 // set user arg[0] and repo arg[1]
 // 2do: User os.FileSeperator ...
 // 2do: run headcom / playgrounder etc. pp.
-
-// Move location/user/repo to corpus/user/repo
-
-// checkin stuff
+// 2do: change "gobeyd"
 
 def tempDir = 'd:\\temp\\gitemp'
 def corpusDir = 'd:\\dbt\\01\\git\\gobeys\\corpus'
@@ -16,33 +13,24 @@ def repo = this.args[1]
 
 println "Sync: " + provider + user + "/" + repo
 
-
 //clone
 cloneCmd = cloneCmd + provider + user + "/" + repo
 def proc = cloneCmd.execute()
 proc.waitFor()
 
-/*
-//desync
-def gitDir = new File(repo + '\\.git') 
-def result = gitDir.deleteDir()  // Returns true if all goes well, false otherwise.
-assert result
-*/
-
 //sloc
 def slocCmd = 'cmd /c sloc ' + repo + ' > ' + repo + '\\sloc.log'
-slocCmd.execute()
+def procSloc = slocCmd.execute()
+procSloc.waitFor()
 
 
 ant = new AntBuilder()
 ant.sequential {
   delete(file:repo + '\\.gitignore')  
   delete(dir:repo + '\\.git')  
-  move (todir: tempDir) {
+  move (todir: corpusDir + "\\" + user + "\\" + repo) {
     fileset (dir: repo)
   }
 }
 
-
-
-println "done."
+println "Sync: done."
